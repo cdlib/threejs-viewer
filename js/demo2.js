558729d544d924962e0a550c907e61969fdb55c0
JoelCDL <joel.hagedorn@ucop.edu>
2020-08-25 16:06:18 -0700

    move routing js to separate file, import into demo js files

--- js/demo2.js
+++ js/demo2.js
@@ -1,32 +1,11 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as routing from './routing.js'
 
 function init () {
-  /* Test Models. Download from URLs below into ./models */
-
-  const urlParams = new URLSearchParams(window.location.search)
-
-  let model = ''
-
-  if (urlParams.get('model') === '1') {
-    // George Washington downloaded from: https://3d.si.edu/object/3d/george-washington:789cf90a-4387-4ac1-9e96-c7d6a7b9d26f
-    model = 'models/george-washington/george-washington-greenough-statue-(1840)-150k-4096-web.gltf'
-  } else if (urlParams.get('model') === '2') {
-    // Blue Crab downloaded from: https://3d.si.edu/object/3d/blue-crab:b0bf6d44-af22-40dc-bd85-7d66255be4a7
-    model = 'models/blue-crab/blue-crab-150k-4096-web.gltf'
-  } else if (urlParams.get('model') === '3') {
-    // Side Chair downloaded from: https://3d.si.edu/object/3d/side-chair:57d30b85-3549-40dc-99c3-255249867462
-    model = 'models/side-chair/pergolesi_side_chair_mesh-100k-web.gltf'
-  } else if (urlParams.get('model') === '4') {
-    // Apolo Interior downloaded from: https://3d.si.edu/object/3d/command-module-apollo-11:d8c63e8a-4ebc-11ea-b77f-2e728ce88125
-    model = 'models/apollo-interior/apollo_interior-medium_resolution.gltf'
-  } else if (urlParams.get('model') === '5') {
-    // Avocado GLTF downloaded from: https://poly.google.com/view/196c4VRe6Ch
-    model = 'models/avocado/Avocado.gltf'
-  } else {
-    document.querySelector('.container').innerText = 'Invalid model parameter or number in URL query string.'
-  }
+  routing.router()
+  const modelUrl = routing.modelUrl
 
   /* Camera, Scene, and Renderer */
 
@@ -124,7 +103,7 @@ function init () {
 
   const gltfLoader = new GLTFLoader()
 
-  gltfLoader.load(model, (gltf) => {
+  gltfLoader.load(modelUrl, (gltf) => {
     const root = gltf.scene
 
     scene.add(root)
---
+++ js/routing.js
@@ -0,0 +1,27 @@
+/* Model Routing */
+
+function router () {
+  if (urlParams.get('model') === '1') {
+    // George Washington downloaded from: https://3d.si.edu/object/3d/george-washington:789cf90a-4387-4ac1-9e96-c7d6a7b9d26f
+    modelUrl = 'models/george-washington/george-washington-greenough-statue-(1840)-150k-4096-web.gltf'
+  } else if (urlParams.get('model') === '2') {
+    // Blue Crab downloaded from: https://3d.si.edu/object/3d/blue-crab:b0bf6d44-af22-40dc-bd85-7d66255be4a7
+    modelUrl = 'models/blue-crab/blue-crab-150k-4096-web.gltf'
+  } else if (urlParams.get('model') === '3') {
+    // Side Chair downloaded from: https://3d.si.edu/object/3d/side-chair:57d30b85-3549-40dc-99c3-255249867462
+    modelUrl = 'models/side-chair/pergolesi_side_chair_mesh-100k-web.gltf'
+  } else if (urlParams.get('model') === '4') {
+    // Apolo Interior downloaded from: https://3d.si.edu/object/3d/command-module-apollo-11:d8c63e8a-4ebc-11ea-b77f-2e728ce88125
+    modelUrl = 'models/apollo-interior/apollo_interior-medium_resolution.gltf'
+  } else if (urlParams.get('model') === '5') {
+    // Avocado GLTF downloaded from: https://poly.google.com/view/196c4VRe6Ch
+    modelUrl = 'models/avocado/Avocado.gltf'
+  } else {
+    document.querySelector('.container').innerText = 'Invalid model parameter or number in URL query string.'
+  }
+}
+
+const urlParams = new URLSearchParams(window.location.search)
+let modelUrl = ''
+
+export { router, urlParams, modelUrl }

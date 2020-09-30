307ad41aee0874eb408598c0f1e5fc9477289f76
JoelCDL <joel.hagedorn@ucop.edu>
2020-09-29 17:18:52 -0700

    feature inst objects on main page, move wip viewers to prototype page

--- js/intro.js
+++ js/intro.js
@@ -1,7 +1,5 @@
 import models from './models.json'
 
-/* Demo Link Generation */
-
 const linksTemplate = document.querySelector('#linkstemplate')
 
 function init () {
@@ -10,33 +8,60 @@ function init () {
   const debugLink = linksTemplateContent.querySelector('.debuglink')
   const modelsArray = models.model
 
-  // Demo 2 links:
+  // Initial Prototype Viewer:
 
-  const demo2Links = document.querySelector('#demo2links')
+  if (document.querySelector('#demo2links')) {
+    const demo2Links = document.querySelector('#demo2links')
 
-  modelsArray.forEach(function (theModel, index) {
-    const modelHref = 'demo2.html?model=' + (index + 1)
-    demoLink.setAttribute('href', modelHref)
-    demoLink.innerText = theModel.name
+    modelsArray.forEach(function (theModel, index) {
+      const modelHref = 'demo2.html?model=' + (index + 1)
+      demoLink.setAttribute('href', modelHref)
+      demoLink.innerText = theModel.name
 
-    demo2Links.appendChild(document.importNode(linksTemplateContent, true))
-  })
+      demo2Links.appendChild(document.importNode(linksTemplateContent, true))
+    })
+  }
 
-  // Demo 3 links:
+  // Current Prototype Viewer:
 
-  const demo3Links = document.querySelector('#demo3links')
+  if (document.querySelector('#demo2links')) {
+    const demo3Links = document.querySelector('#demo3links')
 
-  modelsArray.forEach(function (theModel, index) {
-    const modelHref = 'demo3.html?model=' + (index + 1)
-    demoLink.setAttribute('href', modelHref)
-    demoLink.innerText = theModel.name
+    modelsArray.forEach(function (theModel, index) {
+      const modelHref = 'demo3.html?model=' + (index + 1)
+      demoLink.setAttribute('href', modelHref)
+      demoLink.innerText = theModel.name
 
-    const debugHref = modelHref + '&debug=true'
-    debugLink.setAttribute('href', debugHref)
-    debugLink.hidden = false
+      const debugHref = modelHref + '&debug=true'
+      debugLink.setAttribute('href', debugHref)
+      debugLink.hidden = false
 
-    demo3Links.appendChild(document.importNode(linksTemplateContent, true))
-  })
+      demo3Links.appendChild(document.importNode(linksTemplateContent, true))
+    })
+  }
+
+  // Featured Objects from Institution:
+
+  if (document.querySelector('#featuredlinks')) {
+    const institution = 'UC Merced' // must match institution value exactly within models.json
+    const featuredLinks = document.querySelector('#featuredlinks')
+    const institutionHeading = document.querySelector('#institutionheading')
+
+    modelsArray.forEach(function (theModel, index) {
+      if (theModel.institution === institution) {
+        const modelHref = 'demo3.html?model=' + (index + 1)
+        demoLink.setAttribute('href', modelHref)
+        demoLink.innerText = theModel.name
+        institutionHeading.innerText = theModel.institution
+      } else {
+        return
+      }
+
+      debugLink.hidden = true
+
+      featuredLinks.appendChild(document.importNode(linksTemplateContent, true))
+    })
+  }
 }
 
 if (linksTemplate) {
